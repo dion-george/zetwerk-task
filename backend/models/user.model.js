bccbc34dc63079f29105694ff39b85947e4e9711
diongeorge97 <diongeorge@gmail.com>
2019-11-27 18:21:17 +0530

    backend setup

---
+++ backend/models/user.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3
+  },
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: Date, required: true },
+}, {
+  timestamps: true,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
